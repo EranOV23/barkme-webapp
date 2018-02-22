ad585c557844d8f4c6947e7d82cde39a8c8ca100
Eran Ovadia <eran@meetsnappy.com>
2018-02-22 23:38:00 +0200

    change uri

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:9090/graphql" }),
+  link: new HttpLink({ uri: "https://barkme-server.herokuapp.com/graphql" }),
   cache: new InMemoryCache()
 });
 
